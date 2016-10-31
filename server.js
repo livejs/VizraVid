13e6733b2bc2513e9c1e61642973b9014ac35b5e
Ruth John <rumyra@gmail.com>
2016-10-31 20:52:33 +0000

    😱

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const htmling = require('htmling');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('html', htmling.express(__dirname + '/views/', {watch:true}));
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(process.env.PORT || 3000);
+
